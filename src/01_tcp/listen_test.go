3edaff61676dc960a0552734a7c5bb02212f306c
Dohyun Kim <10915235+rurumimic@users.noreply.github.com>
2022-04-02 17:08:08 +0900

    random port

--- src/01_tcp/listen_test.go
+++ src/01_tcp/listen_test.go
@@ -1,4 +1,4 @@
-// go test -v listen_test.go
+// go test listen_test.go -v
 
 package main
 
@@ -9,6 +9,8 @@ import (
 
 func TestListener(t *testing.T) {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	// listener, err := net.Listen("tcp", ":")
+	// listener, err := net.Listen("tcp", "192.168.101.101:8888")
 
 	if err != nil {
 		t.Fatal(err)
@@ -24,9 +26,21 @@ func TestListener(t *testing.T) {
 /*
 
 === RUN   TestListener
+    listen_test.go:23: bound to "[::]:56471"
+--- PASS: TestListener (0.00s)
+PASS
+ok  	command-line-arguments	0.300s
+
+=== RUN   TestListener
     listen_test.go:21: bound to "127.0.0.1:56411"
 --- PASS: TestListener (0.00s)
 PASS
 ok  	command-line-arguments	0.454s
 
+=== RUN   TestListener
+    listen_test.go:16: listen tcp 192.168.101.101:8888: bind: can't assign requested address
+--- FAIL: TestListener (0.00s)
+FAIL
+FAIL	command-line-arguments	0.298s
+FAIL
 */
